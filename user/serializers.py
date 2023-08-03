86ffa313cfe3c2cd57588e1e11e3e1de3accc87b
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-08-03 16:10:29 +0300

    Added user serializers.py

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,7 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+from .models import UserProfile
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -22,3 +24,59 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.CharField(label=_("Email"))
+    password = serializers.CharField(
+        label=_("Password"), style={"input_type": "password"}
+    )
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if email and password:
+            user = authenticate(email=email, password=password)
+
+            if user:
+                if not user.is_active:
+                    msg = _("User account is disabled.")
+                    raise serializers.ValidationError(msg, code="authorization")
+            else:
+                msg = _("Unable to log in with provided credentials.")
+                raise serializers.ValidationError(msg, code="authorization")
+        else:
+            msg = _("Must include 'username' and 'password'.")
+            raise serializers.ValidationError(msg, code="authorization")
+
+        attrs["user"] = user
+        return attrs
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("email",)
+
+
+class UserProfileListSerializer(serializers.ModelSerializer):
+    owner_email = serializers.EmailField(source="owner.email", read_only=True)
+
+    class Meta:
+        model = UserProfile
+        fields = (
+            "id",
+            "owner",
+            "owner_email",
+            "username",
+            "profile_picture",
+            "bio",
+        )
+        read_only_fields = ("id", "owner")
+
+
+class UserProfileDetailSerializer(UserProfileListSerializer):
+    class Meta(UserProfileListSerializer.Meta):
+        fields = UserProfileListSerializer.Meta.fields + ("following", "followers")
+
